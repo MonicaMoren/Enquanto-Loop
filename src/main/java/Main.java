9ae64b3678f21dbca4a32cd6e5ea00da09a380d5
Monica Moreno <169068483+MonicaMoren@users.noreply.github.com>
2024-07-29 00:04:01 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+  public static void main(String[] args) {
+int Contador = 1;
+    while(Contador < 11){
+System.out.println("Bom Dia");
+      Contador = Contador + 1 ;
+    }
+
+
+    
+  }
+
+}
